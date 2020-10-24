7da353902bf1ed2efcb3688b010ab64476bbc47a
Ben Ferdinandus <4364969+bferdinandus@users.noreply.github.com>
2020-10-24 18:19:40 +0200

    Added a dotnet core winforms example.

---
+++ samples/CoreAudioForms.Core.Sample/FormMain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+using CoreAudio;
+
+namespace CoreAudioForms.Core.Sample
+{
+    public partial class FormMain : Form
+    {
+        private readonly MMDevice _device;
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            MMDeviceEnumerator devEnum = new MMDeviceEnumerator();
+            _device = devEnum.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia);
+            tbMaster.Value = (int) (_device.AudioEndpointVolume.MasterVolumeLevelScalar * 100);
+            _device.AudioEndpointVolume.OnVolumeNotification += AudioEndpointVolume_OnVolumeNotification;
+        }
+
+        private void AudioEndpointVolume_OnVolumeNotification(AudioVolumeNotificationData data)
+        {
+            if (InvokeRequired)
+            {
+                object[] Params = new object[1];
+                Params[0] = data;
+                Invoke(new AudioEndpointVolumeNotificationDelegate(AudioEndpointVolume_OnVolumeNotification), Params);
+            }
+            else
+            {
+                tbMaster.Value = (int) (data.MasterVolume * 100);
+            }
+        }
+
+        private void Update_Timer_Tick(object sender, EventArgs e)
+        {
+            pbMaster.Value = (int) (_device.AudioMeterInformation.MasterPeakValue * 100);
+            pbLeft.Value = (int) (_device.AudioMeterInformation.PeakValues[0] * 100);
+            pbRight.Value = (int) (_device.AudioMeterInformation.PeakValues[1] * 100);
+        }
+
+        private void Master_Scroll(object sender, EventArgs e)
+        {
+            _device.AudioEndpointVolume.MasterVolumeLevelScalar = (tbMaster.Value / 100.0f);
+        }
+    }
+}
